7b289491a60d45e3a97655907b111b88496819c7
cahlimbah <55645236+cahlimbah@users.noreply.github.com>
2019-09-23 12:33:46 +0700

    Update autombf.py

--- autombf.py
+++ autombf.py
@@ -2,8 +2,8 @@
 
 """
 * Jangan Recode Ya Goblok
-* Author : V4N654T
-* Github : https://github.com/V4N654T
+* Author : CAHLimbah
+* Github : https://github.com/cahlimbah
 -------------------------------------------------------
 * Name Tools : Auto MBF
 * Full Name : Auto Multi-Bruteforce-Facebook
